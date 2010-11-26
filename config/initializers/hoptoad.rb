52d741423b2dc96ecd16dfb7b20a4c9faf38fbe3
mat_aki <matsumura.aki@gmail.com>
2010-11-26 11:35:04 +0900

    Devide hoptoad project from youRoom

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,1 +1,4 @@
-require "#{GURUGURU_PATH}/config/initializers/hoptoad"
+HoptoadNotifier.configure do |config|
+  config.api_key = "28b2a77fed5cf2e605fbb7d709187c62"
+  config.ignore << "ActionController::MethodNotAllowed"
+end
